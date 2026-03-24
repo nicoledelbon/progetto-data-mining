6c0b967151124f74e997cbb5e7a33468715a7092
sofiaamodeo <sofia.amodeo04@gmail.com>
2026-03-24 12:59:42 +0100

    Add files via upload

--- code.R
+++ code.R
@@ -1,1 +1,5 @@
-print("ciao")
+library(readr)
+spotify <- read_csv("Most Streamed Spotify Songs 2024.csv", 
+                    col_types = cols(`Release Date` = col_date(format = "%m/%d/%Y"), 
+                                     `TIDAL Popularity` = col_skip(), 
+                                     `Explicit Track` = col_logical()))
