10feb921e713fa54dfe4cafa9891b79415fde96d
Sofia Amodeo <sofia.amodeo04@gmail.com>
2026-03-30 11:12:11 +0200

    g

--- code.R
+++ code.R
@@ -3,10 +3,5 @@ rm(list=ls())
 library(readr)
 student <- read_csv("student_performance_updated_1000.csv")
 sum(is.na(student))
-=======
-data <- read.csv("IMDbMovies-Clean.csv", header = TRUE)
-data[data==""] <- NA
-sum(is.na(data))
->>>>>>> b5ae94f880c3d010ae623ff3093fdd57c495ce25
 
 student=student[,-c(1,2)]
