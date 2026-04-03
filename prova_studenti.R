2c28011569cc232415b5170f688247dbd524c1ac
soniapelizzari5 <soniapelizzari5@gmail.com>
2026-04-03 10:54:12 +0200

    ,

--- prova_studenti.R
+++ prova_studenti.R
@@ -13,6 +13,21 @@ f1_score <- function(cm) {
 }
 
 student_dropout_dataset_v3 <- read.csv("~/progetto-data-mining/student_dropout_dataset_v3.csv")
+
+# Obiettivo della mia analisi
+# La nostra analisi ha lo scopo di prevedere se uno studente abbandona il suo ciclo di studi prima di terminarlo.
+# Al fine di prevedere l'abbandono, abbiamo analizzato un dataset contenente diverse informazioni,
+# sia sui dati personali dello studente, sulla sua educazione e studi precedenti, ma anche sulla famiglia
+# e dove vive lo studente e le possibilità che ha a disposizione ( es: accesso a Internet)
+
+summary(student_dropout_dataset_v3)
+# summary per capire le variabili in esame
+# Ho dei valori mancanti nelle variabili Family_Income (dato sensibile)
+# Study_Hours_per_Day, Stress_index
+# I range di variazione delle variabili sono coerenti con le variabili
+# in esame
+
+
 data <- student_dropout_dataset_v3[,-1]
 data$Gender <- as.factor(data$Gender)
 data$Internet_Access <- as.factor(data$Internet_Access)
@@ -21,9 +36,59 @@ data$Department <- as.factor(data$Department)
 data$Parental_Education <- as.factor(data$Parental_Education)
 data$Part_Time_Job <- as.factor(data$Part_Time_Job)
 data$Scholarship <- as.factor(data$Scholarship)
+data$Dropout <- as.factor(data$Dropout )
+data$Assignment_Delay_Days <- as.factor(data$Assignment_Delay_Days)
+
+# La variabile ID non è utile alle mie analisi, la rimuovo
+summary(data)
+# Ho delle variabili numeriche e delle variabili dummy
+
+
+# analisi grafica ---------------------------------------------------------
+colnames(data)
+data_numeric <- data[, c("Age","Family_Income"  , "Study_Hours_per_Day" ,"Attendance_Rate"    
+                         , "Travel_Time_Minutes" ,"Stress_Index" , "GPA"                  
+                        ,"Semester_GPA" ,"CGPA", "Dropout")]
+par(mfrow=c(3,3))
+for(i in 1:(ncol(data_numeric)-1)){
+  plot(data_numeric[,i], col=data$Dropout, ylab=colnames(data_numeric)[i],
+       main="")
+}
+par(mfrow=c(1,1))
+
+# visulaizzazione dei missing
+library("visdat")
+library("naniar")
+
+vis_miss(data)
+# I missing sono solo in 3 variabili e sembrano essere tutti nelle stesse osservazioni
+# I missing sono solo in piccola parte, se non trovo un metodo di imputazione valido
+# potrei anche rimuoverli. 
+
+gg_miss_var(data)
+# 500 osservazioni contengono missing in 3 variabili 
+# le altre variabili non ne contengono 
+# (potrebbe essere che queste variabili non erano obbligatorie nella compilazione
+# del questionario)
+
+
+#library("UpSetR")
+#upset(as_shadow_upset(data))
+
+library(mice)
+md.pattern(data)
+
+
 
 table(data$Dropout)  
-# molte più persone continuano
+# Dai dati originali, vedo che molti studenti completano i loro studi, mentre chi abbandona è
+# in numero minore. Siamo quindi in presenza di dati sbilanciati. 
+# Prima dell'imputazione quindi, al fine di evitare distorsioni
+# nelle nostre analisi, abbiamo pensato di dividere i dati originali in train e test set, e 
+# provare ad eseguire oversampling, undersampling ed eseguire le analisi sui dati originali.
+# Per ogni scenario abbiamo imputato i dati in diverse modalità (pmm - cart - norm - mean)
+# e valutato il risultato migliore.
+
 
 # provo con i dati cosi, under e oversampling -----------------------------
 
@@ -196,6 +261,19 @@ accuracies
 f1
 # 0.8934373 0.8943776 0.8934373 0.8937277
 
+
+# - -----------------------------------------------------------------------
+
+# L'oversampling è la tecnica che dà risultati migliori, considerando sia l'accuracy che  F1_score,
+# metrica più adatta a valutare la precisione delle stime in presenza di dati sbilanciati.
+# In particolare, Il metodo mean risulta il migliore in termini di imputazione.
+# Abbiamo quindi eseguito un oversampling sulla totalità dei dati originali e imputato
+# i valori mancanti tramite mean.
+
+# - -----------------------------------------------------------------------
+
+
+
 # --> migliore è mean
 
 # imputazione di tutti i valori con mean e oversampling -------------------
@@ -210,6 +288,8 @@ data$Department <- as.factor(data$Department)
 data$Parental_Education <- as.factor(data$Parental_Education)
 data$Part_Time_Job <- as.factor(data$Part_Time_Job)
 data$Scholarship <- as.factor(data$Scholarship)
+data$Dropout <- as.factor(data$Dropout )
+data$Assignment_Delay_Days <- as.factor(data$Assignment_Delay_Days)
 
 
 lab <- sample(1:2354, 7646, replace=T)
@@ -233,19 +313,67 @@ densityplot(data_imp)
 #modelFit <- with(data_imp,lm(Dropout~ .))
 #summary(pool(modelFit))
 
+
+
+
+# selezione variabili se necessaria ---------------------------------------
 dati <- complete(data_imp, 1)
+dati_numeric <- dati[, c("Age","Family_Income"  , "Study_Hours_per_Day" ,"Attendance_Rate" ,     
+                          "Travel_Time_Minutes" ,"Stress_Index" , "GPA"                  
+                         ,"Semester_GPA" ,"CGPA", "Dropout")]
+
+cor(dati_numeric[,-10])
+library(ggcorrplot)
+ggcorrplot(dati_numeric[,-10])
+# NON SO CHE PROBLEMI ABBIA
+
+# Le variabili Semester GPA, CGPA e GPA sono collineari
+# infatti sono tutti indicatori della media dei voti. 
+# applico una selezione della variabili
+
+
+
+# raga HELP --------------------------------------------------------------------
+
+# QUA IL MDELLO LE TIWNE TUTTE, FACCIAMO NOI UNA SELEZIONE?
+# PRENDIAMO MEDIA CGPA CHE PENSO SIA TITPO TOTALE?
+
+mnull <- glm(dati$Dropout ~ 1, data=dati, family="binomial")
+mfull <- glm(dati$Dropout ~ ., data=dati, family="binomial")
+step(mfull, mnull, direction="both")
+
+
+# Tengo solo un valore per la media, al fine di evitare dati ridondandi
+dati <- dati[, !(names(dati) %in% c("Semester_GPA", "GPA"))]
+
+
+# Classificazione ---------------------------------------------------------
+
+# Voglio prevedere l'abbandono o meno di uno studente. Per fare ciò, eseguo una classificazione
+# dei dati in base alla variabile Dropout. Se ottengo 0 significa che lo studente proseguirà i suoi
+# studi, 1 invece indica l'abbandono.
+# Decido di selezionare il primo set di dati ottenuto dalla prima imputazione (?).
+# Divido i dati nuovamente in test e train, e valuto il miglior modello di classificazione.
+
 
 lab1 <- sample(1:nrow(dati), 0.2*nrow(dati))
 dati_trn <- dati[-lab1,]
 dati_tst <- dati[lab1,]
-dati_trn_no_dropout <- dati[-lab1,-18]
-dati_tst_no_dropout <- dati[lab1,-18]
-x_train = dati_trn[,-18]
-x_test = dati_tst[,-18]
-y_train <- dati_trn[,18]
-y_test <- dati_tst[, 18]
+dati_trn_no_dropout <- dati[-lab1,-16]
+dati_tst_no_dropout <- dati[lab1,-16]
+x_train = dati_trn[,-16]
+x_test = dati_tst[,-16]
+y_train <- dati_trn[,16]
+y_test <- dati_tst[, 16]
 
+dati_trn_num <- dati_trn[, c("Age","Family_Income"  , "Study_Hours_per_Day" ,"Attendance_Rate" ,     
+                              "Travel_Time_Minutes" ,"Stress_Index","CGPA", "Dropout")]
 
+# visualizzazione dei dati imputati di train
+library(GGally)
+ggpairs(dati_trn_num[,-8], aes(color = as.factor(dati_trn_num$Dropout)))
+# l'assunzione di normalità non è proprio verificata, non mi aspetto che
+# LDA sia il miglior metodo
 
 # LDA ---------------------------------------------------------------------
 
