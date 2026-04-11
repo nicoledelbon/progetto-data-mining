3c98dd0ce21b17ece485bc6f19f7acc6828d9d2c
Nicole Delbono <nicoledelbono@MacBook-Pro-di-nicole.local>
2026-04-11 09:48:53 +0200

    cc

--- prova_studenti.R
+++ prova_studenti.R
@@ -130,6 +130,7 @@ my_predictorMatrix[ ,18] <- 0
 library(mice)
 my_training1 <- mice(train, method = "pmm", predictorMatrix = my_predictorMatrix, seed = 1234, printFlag = FALSE)
 my_training2 <- mice(train, method = "mean", predictorMatrix = my_predictorMatrix, seed = 1234,  printFlag = FALSE)
+
 my_training3 <- mice(train, method = "norm", predictorMatrix = my_predictorMatrix, seed = 1234,  printFlag = FALSE)
 my_training4 <- mice(train, method = "cart", predictorMatrix = my_predictorMatrix, seed = 1234, printFlag = FALSE)
 
